8bc789ad66e4b9f6cedac85d73e1956675b9b0e4
KUN309 <cung.buihoangcung@hcmut.edu.vn>
2025-12-04 01:56:29 +0700

    Implement share API in bot

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,5 +1,41 @@
 package main
 
-func main(){
-	
+import (
+	"fmt"
+	"log"
+
+	"fe-file-sharing/internal/api"
+	"fe-file-sharing/internal/bot"
+	"fe-file-sharing/internal/config"
+)
+
+func main() {
+	// Load config
+	config.Load("")
+
+	fmt.Println("Tải config thành công")
+
+	// Create API client (NO MORE ForBot)
+	client := api.NewClient(
+		config.C.BackendAPIBase,
+		0,          // bot không có telegramID
+		"telegram", // username tạm
+		nil,
+	)
+
+	fmt.Println("Tạo client mới thành công")
+
+	// Create bot
+	teleBot, err := bot.NewBot(
+		config.C.TelegramBotToken,
+		client,
+	)
+	if err != nil {
+		log.Fatalf("Tạo bot mới thất bại: %v", err)
+	}
+	fmt.Println("Tạo bot mới thành công")
+
+	// Start bot
+	teleBot.Start()
+	fmt.Println("Bot đã bắt đầu chạy")
 }
